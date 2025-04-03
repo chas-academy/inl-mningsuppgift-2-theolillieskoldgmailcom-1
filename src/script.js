7c4ce907fa9fec37fa42c25d53ed7f26dcc86eb7
theo.lillieskold@gmail.com <theo.lillieskold@gmail.com>
2025-04-03 20:41:30 +0200

    first commit

--- src/script.js
+++ src/script.js
@@ -1,20 +1,38 @@
 // Definera array av namn
-let people;
+let people = [
+  "Inez",
+  "Theo",
+  "Kevin",
+  "Felicia"
+]
 
 
 export function initSearch(people) { // rör ej denna rad
 
   // Hämta HTML-element
+  const htmlInput = document.getElementById("searchInput")
+  const htmlButton = document.getElementById("searchBtn")
+  const htmlResult = document.getElementById("resultDisplay")
   
   // Lägg till en klickhändelse på "Sök"-knappen
   searchBtn.addEventListener("click",() => {
   
     // 1. Hämta texten i searchInput.value
-    
+    let writtenName = htmlInput.value
     // 2. Skapa en boolean för found
-    
+    let found;
     // 3. Loopa igenom people-arrayen
-    
+    for(name of people){
+      if(writtenName === name){
+        found = true
+        htmlResult.innerHTML = "Namn hittades!"
+      }
+      else if(found != true){
+        found = false
+        htmlResult.innerHTML = "Namn hittades inte"
+      }
+     
+    }
     // 4. Jämför texten med varje namn i arrayen
     
     // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
